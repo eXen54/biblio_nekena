2bf7a5a1b6410f3bfe622166bc61abcca24d832f
eXen54 <nekena.tsaratsiry@gmail.com>
2025-07-03 14:47:50 +0300

    liste livres

---
+++ bibliotheque/src/main/java/com/example/bibliotheque/controller/LivreController.java
@@ -0,0 +1,20 @@
+package com.example.bibliotheque.controller;
+
+import com.example.bibliotheque.repository.LivreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LivreController {
+
+    @Autowired
+    private LivreRepository livreRepository;
+
+    @GetMapping("/livres")
+    public String afficherLivres(Model model) {
+        model.addAttribute("livres", livreRepository.findAll());
+        return "livres";
+    }
+}
